2c4f2bd83f9591a4d0c7a76244217c3c192422b7
Vladyslav Mykhailichenko <mykhailichenkovlad@gmail.com>
2023-05-10 15:35:09 +0300

    Added wite int file

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Diagnostics.Metrics;
 using System.Runtime.InteropServices;
 using System.Xml;
@@ -12,80 +13,18 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        #region delegate
-        var students = new List<Student>
-        {
-            new Student(),
-            new Student(),
-            new Student(),
-            new Student(),
-            new Student(),
-            new Student(),
-            new Student(),
-            new Student(),
-            new Student(),
-            new Student(),
-            new Student(),
-            new Student(),
-            new Student()
-        };
+        #region write data in file        
+        Group group = new();
 
-        Console.WriteLine("Unsorted:");
-        foreach (var student in students)
+        using (StreamWriter writer = new StreamWriter(@"D:\c#-projects\EducationInstitution\EducationInstitution\StudentsData.txt"))
         {
-            Console.WriteLine($"{student.Name} - {student.GetAverageMark()}");
+            foreach (Student student in group)
+            {
+                writer.WriteLine("{0}, {1}, {2}, {3}", student.Name, student.Birthday, student.Address, student.PhoneNumber);
+            }
         }
 
-        SortStudents(students, RateCriteria);
-        Console.WriteLine();
-
-        Console.WriteLine("Sorted:");
-        foreach (var student in students)
-        {
-            Console.WriteLine($"{student.Name} - {student.GetAverageMark()}");
-        }
-        #endregion
-
-        #region events       
-        Student studentEvents = new Student("Vladislav", "Mykhailichenko", "Sergeevich");
-        Console.WriteLine("\n" + studentEvents);
-
-        studentEvents.Overslept += (sender, e) =>
-        {
-            Console.WriteLine("Teacher: Why are you late?");
-            Console.WriteLine("Student: I overslept...");
-        };
-
-        studentEvents.GotMachineGun += (sender, e) =>
-        {
-            Console.WriteLine("Police: Put down your weapon!");
-            Console.WriteLine("Student: I'm just joking, it's a toy gun...");
-        };
-
-        studentEvents.PassedExamOn12 += (sender, e) =>
-        {
-            Console.WriteLine("Parent: Congratulations on your excellent grade!");
-            Console.WriteLine("Student: Thanks, but it was just luck...");
-        };
-
-        Console.WriteLine("Something happens to the student...");
-        studentEvents.SleepIn();
-        Console.WriteLine();
-
-        Console.WriteLine("Something else happens to the student...");
-        studentEvents.GetWeapon();
-        Console.WriteLine();
-
-        Console.WriteLine("Yet another thing happens to the student...");
-        studentEvents.PassExam();
-        Console.WriteLine();
+        Console.WriteLine("Data written to file.");
         #endregion
     }
-
-    static void SortStudents(List<Student> students, Comparison<Student> comparison)
-    {
-        students.Sort(comparison);
-    }
-
-    static int RateCriteria(Student a, Student b) => b.GetAverageMark().CompareTo(a.GetAverageMark());
 }
